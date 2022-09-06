1e24df4b3af60eb11dc65f78c973df9053c8bf02
Mustafasul <Mustafasul.cs2020@gmail.com>
2022-09-06 09:27:23 +0430

    this is Connection between php and mysql server

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "py_store";
+ 
+   $conn = mysqli_connect($servername , $username , $password , $database);
+
+    
+    // if ($conn) {
+    //  	echo "connection successfully done.....";
+    //  }
+    //  else {
+    //  	echo "connection error .....";
+    //  }
+
+
+
+
+ ?>
